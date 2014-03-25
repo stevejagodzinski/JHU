af3864f7114ada2f1e6e8bd40dce8ae1d50a85c1
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-03-24 22:14:25 -0400

    update js

--- 787/HW8/src/main/webapp/scripts/sjagodz18_simple_banking.js
+++ 787/HW8/src/main/webapp/scripts/sjagodz18_simple_banking.js
@@ -1,18 +1,8 @@
-function findCustomersByIDs(outputFormat) {
-    var baseAddress = "CustomerLookup";
-    var data = "customerIds=" + getValue('customerIds') + "&outputFormat=" + outputFormat;
+function findCustomersByIDs() {
+    var baseAddress = "GetCustomersByIDs";
+    var data = "customerIds=" + getValue('customerIds');
     var address = baseAddress + "?" + data;
-    ajaxResult(address, 'findCustomersByIdsResult', getAjaxResultHandler(outputFormat));
-}
-
-function getAjaxResultHandler(outputFormat) {
-	if(outputFormat == 'JSON') {
-		return jsonAjaxResultHandler;
-	} else if(outputFormat == 'STRING') {
-		return stringAjaxResultHandler;
-	}  else if(outputFormat == 'XML') {
-		return xmlAjaxResultHandler;
-	}
+    ajaxResult(address, 'findCustomersByIdsResult', jsonAjaxResultHandler);
 }
 
 function jsonAjaxResultHandler(request, resultRegion) {
@@ -22,70 +12,6 @@ function jsonAjaxResultHandler(request, resultRegion) {
 		var table = buildTable(json);
 		htmlInsert(resultRegion, table);
 	}
-}
-
-function stringAjaxResultHandler(request, resultRegion) {
-	if ((request.readyState == 4) &&
-		      (request.status == 200)) {
-		var customersArray = new Array();
-		var customerStrings = request.responseText.split('\n');
-		for(var i = 0; i < customerStrings.length; i++) {
-			customersArray.push(parseCustomerString(customerStrings[i]));
-		}
-		htmlInsert(resultRegion, buildTable(customersArray));
-	}
-}
-
-function parseCustomerString(customerString) {
-	var customerSubString = customerString.substring(customerString.indexOf('[')+1, customerString.indexOf(']'));
-	var fields = customerSubString.split(',');
-	var customer = new Object();
-	for(var i = 0; i < fields.length; i++) {
-		var keyValue = fields[i].split('=');
-		customer[keyValue[0]] = keyValue[1];
-	}
-	return customer;
-}
-
-
-function xmlAjaxResultHandler(request, resultRegion) {
-	if ((request.readyState == 4) &&
-		      (request.status == 200)) {
-		var customersArray = buildCustomerArrayFromXML(request.responseXML);
-		var table = buildTable(customersArray);
-		htmlInsert(resultRegion, table);
-	}
-}
-
-function buildCustomerArrayFromXML(xmlDocument) {
-	var customerArray = new Array();
-
-	var customerIds = getXmlValues(xmlDocument, 'customerId');
-
-	for(var i = 0; i < customerIds.length; i++) {
-		customerArray.push(new Object());
-		customerArray[i].customerId = customerIds[i];
-	}
-
-	var firstNames = getXmlValues(xmlDocument, 'firstName');
-
-	for(var i = 0; i < firstNames.length; i++) {
-		customerArray[i].firstName = firstNames[i];
-	}
-
-	var lastNames = getXmlValues(xmlDocument, 'lastName');
-
-	for(var i = 0; i < lastNames.length; i++) {
-		customerArray[i].lastName = lastNames[i];
-	}
-
-	var accountBalances = getXmlValues(xmlDocument, 'accountBalance');
-
-	for(var i = 0; i < accountBalances.length; i++) {
-		customerArray[i].accountBalance = accountBalances[i];
-	}
-
-	return customerArray;
 }
 
 function buildTable(customers) {
