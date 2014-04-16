077ccd903930b874d27b169015fdfef6e9dd379f
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-04-15 21:37:07 -0400

    Google Search on city

--- 787/HW10/src/main/webapp/scripts/sjagodz110_cities_remote.js
+++ 787/HW10/src/main/webapp/scripts/sjagodz110_cities_remote.js
@@ -1,3 +1,12 @@
 window.onload = function() {
 	new Ajax.Autocompleter("cityInputAjax", "cityMenuDivAjax", "GetCities");
 };
+
+function googleSearch(id) {
+	var language = getValue(id);
+	window.location.href = "http://www.google.com/search?q=" + language;
+}
+
+function getValue(id) {
+	return (escape(document.getElementById(id).value));
+}
