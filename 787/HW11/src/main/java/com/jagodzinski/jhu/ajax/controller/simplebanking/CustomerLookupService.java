33065dd13392f55bfbc9d19b5f709e7a24fbddc8
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-04-22 22:11:54 -0400

    Remove unused methods from customer lookup service

--- 787/HW11/src/main/java/com/jagodzinski/jhu/ajax/controller/simplebanking/CustomerLookupService.java
+++ 787/HW11/src/main/java/com/jagodzinski/jhu/ajax/controller/simplebanking/CustomerLookupService.java
@@ -1,11 +1,7 @@
 package com.jagodzinski.jhu.ajax.controller.simplebanking;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import com.jagodzinski.jhu.ajax.model.simplebanking.CustomerAccountSummary;
@@ -59,16 +55,7 @@ public class CustomerLookupService
         customerAccountSummary.setAccountBalance(new BigDecimal("123.00"));
 
         customers.put(customerAccountSummary.getCustomerId(), customerAccountSummary);
-    }
 
-    public Collection<CustomerAccountSummary> getAllCustomers(final Collection<String> customerIds) {
-    	List<CustomerAccountSummary> customerAccountSummaries = new ArrayList<>(customerIds.size());
-    	
-    	for(String customerId : customerIds) {
-    		customerAccountSummaries.add(getCustomerById(customerId));
-    	}
-    	
-    	return customerAccountSummaries;
     }
     
     public CustomerAccountSummary getCustomerById(final String customerId)
@@ -82,66 +69,4 @@ public class CustomerLookupService
 
 		return customer;
     }
-
-    public List<String> getCustomerIds()
-    {
-        List<String> customerIds = new ArrayList<>(customers.keySet());
-        Collections.sort(customerIds);
-        return Collections.unmodifiableList(customerIds);
-    }
-
-    public CustomerAccountSummary getCustomerByName(final String firstName, final String lastName)
-    {
-        CustomerAccountSummary returnValue = null;
-
-        for(CustomerAccountSummary customer : customers.values())
-        {
-            if(customer.getFirstName().equalsIgnoreCase(firstName) && customer.getLastName().equalsIgnoreCase(lastName))
-            {
-                returnValue = customer;
-                break;
-            }
-        }
-
-        return returnValue;
-    }
-
-	public List<String> getAllCustomerIdsStartingWith(String prefix)
-	{
-		prefix = prefix.toUpperCase();
-
-		List<String> matches = new ArrayList<>();
-
-		for (CustomerAccountSummary customer : customers.values())
-		{
-			String customerId = customer.getCustomerId();
-			if (customerId.toUpperCase().startsWith(prefix))
-			{
-				matches.add(customerId);
-			}
-		}
-
-		return matches;
-	}
-
-	public CustomerAccountSummary findCustomerWithHighestBalance()
-	{
-		CustomerAccountSummary highestBalanceCustomer = null;
-
-		for (CustomerAccountSummary customer : customers.values())
-		{
-			if (hasHigherBalance(highestBalanceCustomer, customer))
-			{
-				highestBalanceCustomer = customer;
-			}
-		}
-
-		return highestBalanceCustomer;
-	}
-
-	private boolean hasHigherBalance(CustomerAccountSummary highestBalanceCustomer, CustomerAccountSummary toCheck)
-	{
-		return highestBalanceCustomer == null
-				|| toCheck.getAccountBalance().compareTo(highestBalanceCustomer.getAccountBalance()) > 0;
-	}
 }
