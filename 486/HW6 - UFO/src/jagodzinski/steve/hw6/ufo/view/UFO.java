3adaad56c28b25d3e8086add1ae9fc08cd9c84a2
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2014-05-02 20:43:03 -0400

    Show ship paths on map

--- 486/HW6 - UFO/src/jagodzinski/steve/hw6/ufo/view/UFO.java
+++ 486/HW6 - UFO/src/jagodzinski/steve/hw6/ufo/view/UFO.java
@@ -7,21 +7,39 @@ import jagodzinski.steve.hw6.ufo.service.UFOLocationService;
 import jagodzinski.steve.hw6.ufo.service.UFOLocationService.Reporter;
 import jagodzinski.steve.hw6.ufo.service.UFOLocationService.UFOLocationServiceBinder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 public class UFO extends Activity {
 
+	private static final int GOOGLE_PLAY_SETUP = 42;
+
+	private GoogleMap googleMap;
 	private UFOLocationServiceBinder serviceBinder;
+
+	private LatLngBounds bounds = new LatLngBounds.Builder().include(new LatLng(38.9073, -77.0365)).build();
+	private Map<Integer, LatLng> shipLocations = new HashMap<Integer, LatLng>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +52,12 @@ public class UFO extends Activity {
 		Log.d("UFO", "onStart");
 		bindService(new Intent(this, UFOLocationService.class), serviceConnection, Context.BIND_AUTO_CREATE);
 		super.onStart();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		initGoogleMap();
 	}
 
 	@Override
@@ -51,6 +75,49 @@ public class UFO extends Activity {
         return true;
     }
     
+	private boolean initGoogleMap() {
+		boolean success;
+
+		int googlePlayServicesAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		switch (googlePlayServicesAvailable) {
+			case ConnectionResult.SUCCESS:
+				success = true;
+				break;
+			case ConnectionResult.SERVICE_MISSING:
+			case ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED:
+			case ConnectionResult.SERVICE_DISABLED:
+				GooglePlayServicesUtil.getErrorDialog(googlePlayServicesAvailable, this, GOOGLE_PLAY_SETUP).show();
+				success = false;
+				break;
+			default:
+				throw new RuntimeException("Unexpected result code from isGooglePlayServicesAvailable: " + googlePlayServicesAvailable + " ("
+						+ GooglePlayServicesUtil.getErrorString(googlePlayServicesAvailable) + ")");
+		}
+
+		if (success) {
+			MapFragment map = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+			googleMap = map.getMap();
+		}
+
+		return success;
+	}
+
+	private void updateShipLocations(final List<UFOPosition> ufoPositions) {
+		for (UFOPosition ufoPosition : ufoPositions) {
+			LatLng previousPosition = shipLocations.get(ufoPosition.getShipNumber());
+			LatLng currentPosition = new LatLng(ufoPosition.getLat(), ufoPosition.getLon());
+			shipLocations.put(ufoPosition.getShipNumber(), currentPosition);
+			bounds = bounds.including(currentPosition);
+
+			if (previousPosition != null) {
+				googleMap.addPolyline(new PolylineOptions().color(Color.BLUE).width(getResources().getDimension(R.dimen.path_width)).add(previousPosition)
+						.add(currentPosition));
+			}
+		}
+
+		googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, (int) getResources().getDimension(R.dimen.camera_padding)));
+	}
+
 	private ServiceConnection serviceConnection = new ServiceConnection() {
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
@@ -70,7 +137,7 @@ public class UFO extends Activity {
 			runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-					Log.d("MainActivity reporter", ufoPositions.toString());
+					updateShipLocations(ufoPositions);
 				}
 			});
 		}
